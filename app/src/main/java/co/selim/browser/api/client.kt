25d26a4f7f62e9a9fb0d022c4aed344ceadb4342
Selim Dincer <wowselim@live.de>
2023-01-16 16:39:48 +0300

    enable okhttp cache

--- app/src/main/java/co/selim/browser/api/client.kt
+++ app/src/main/java/co/selim/browser/api/client.kt
@@ -2,8 +2,10 @@ package co.selim.browser.api
 
 import co.selim.browser.BuildConfig
 import com.squareup.moshi.Moshi
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -32,6 +34,7 @@ val apiModule = module {
 
                 chain.proceed(authenticatedRequest)
             }
+            .cache(Cache(androidContext().cacheDir, 1024 * 10))
             .build()
 
         val moshi: Moshi = Moshi.Builder()
