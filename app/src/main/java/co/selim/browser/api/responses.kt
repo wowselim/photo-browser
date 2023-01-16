b2411e25d5467584592b0f021c05cddee5e7b0cd
Selim Dincer <wowselim@live.de>
2023-01-16 11:44:50 +0300

    remove now unused network resource type

--- app/src/main/java/co/selim/browser/api/responses.kt
+++ app/src/main/java/co/selim/browser/api/responses.kt
@@ -4,9 +4,3 @@ import retrofit2.Response
 
 val <T> Response<T>.body: T
     get() = checkNotNull(body())
-
-sealed interface NetworkResource<out T> {
-    data class Loaded<T>(val value: T) : NetworkResource<T>
-    object Loading : NetworkResource<Nothing>
-    object Error : NetworkResource<Nothing>
-}
