1fdd19764110ba87d2e407e5195b2217d22d1105
Selim Dincer <wowselim@live.de>
2023-01-19 19:44:46 +0300

    format exif date time

--- app/src/main/java/co/selim/browser/ui/utils/exifKeys.kt
+++ app/src/main/java/co/selim/browser/ui/utils/exifKeys.kt
@@ -4,6 +4,9 @@ import co.selim.browser.R
 import co.selim.browser.model.ExifKey
 import co.selim.browser.model.ExifKey.*
 import com.ioki.textref.TextRef
+import java.time.DateTimeException
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 val ExifKey.displayText: TextRef
     get() = when (this) {
@@ -18,16 +21,29 @@ val ExifKey.displayText: TextRef
         SHUTTER_SPEED -> TextRef.stringRes(R.string.shutter_speed)
     }
 
+
 fun ExifKey.formatValue(value: String): String {
     return when (this) {
         APERTURE -> "F$value"
         SHUTTER_SPEED -> "${value}s"
+        DATE_TIME -> parseDateTimeOrNull(value) ?: value
         FOCAL_LENGTH,
-        DATE_TIME,
         FLASH_STATUS,
         ISO,
         LENS,
         MAKE,
         MODEL -> value
+    }
+}
+
+private val inputDateFormatter = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm")
+private val outputDateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+private fun parseDateTimeOrNull(value: String): String? {
+    return try {
+        val dateTimeSubstring = value.slice(0..15)
+        val dateTime = LocalDateTime.parse(dateTimeSubstring, inputDateFormatter)
+        outputDateFormatter.format(dateTime)
+    } catch (e: DateTimeException) {
+        null
     }
 }
